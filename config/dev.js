dd03b65e566ec12c64617cacc7080d2ed0672c08
Jumoke Boladale-Lawal <76624942+jutivia@users.noreply.github.com>
2022-09-01 21:17:58 +0100

    first commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:"mongodb+srv://juju:ae0QgS91qsz5QziW@cluster0.tzgjbvs.mongodb.net/?retryWrites=true&w=majority",
+  cookieKey: "123123123",
+};
